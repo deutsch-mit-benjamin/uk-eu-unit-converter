7e4908232562444af3d770f05583e45e9e5b0547
deutsch-mit-benjamin <mailservice208@gmail.com>
2026-08-27 21:16:28 +0200

    Refresh offline cache for feet and inches update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "uk-eu-units-v1";
+const CACHE = "uk-eu-units-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
