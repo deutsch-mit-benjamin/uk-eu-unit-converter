79d71a669fb57c9a93426da088e6dfc50a426e70
deutsch-mit-benjamin <mailservice208@gmail.com>
2026-08-28 09:21:50 +0200

    Refresh cache for feet and inches input

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "uk-eu-units-v2";
+const CACHE = "uk-eu-units-v3";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
